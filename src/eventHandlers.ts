cdb5cdf6ebc9c66aa20b92171373949955ff34ae
Jose <joseearronn@gmail.com>
2024-10-07 01:14:02 +0800

    add comments for main and eventHandlers

--- src/eventHandlers.ts
+++ src/eventHandlers.ts
@@ -1,66 +1,75 @@
 import { Operator } from "./enums"
 import Calculator from "./models/calculator"
 
+/**
+ * The functions below are event handlers for the calculator's key presses
+ * 
+ * Each function takes a Calculator instance as a parameter
+ * @param calculator - The Calculator instance storing the calculator's input and state
+ */
+
 export const handleHello = (calculator: Calculator) => {
   if (!calculator.isOn || calculator.isInputDisabled) return
-  
   calculator.displayHello()
-  }
+}
 
 export const handleBye = (calculator: Calculator) => {
   if (!calculator.isOn || calculator.isInputDisabled) return
-  
   calculator.displayBye()
-  }
+}
 
+/**
+ * Handle the 'AC' key press to clear the calculator or turn it on
+ * Clear the calculator if it’s on
+ * Turn the calculator on if it's off
+ */
 export const handleAC = (calculator: Calculator) => {
   if (calculator.isInputDisabled) return
-
   if (calculator.isOn) {
     calculator.clear()
   } else {
     calculator.turnOn()
-    }
   }
+}
 
 export const handleEqualInput = (calculator: Calculator) => {
   if (!calculator.isOn || calculator.isInputDisabled || !calculator.canAddOperator) return
-
   calculator.displayResult(true)
-  } 
+}
 
 export const handleBackspace = (calculator: Calculator) => {
   if (!calculator.isOn || calculator.isInputDisabled) return
-
   calculator.backspace()
-  }
+}
 
+/**
+ * @param number - The number input as a string
+ */
 export const handleNumberInput = (number: string, calculator: Calculator): void => {
   if (!calculator.isOn || calculator.isInputDisabled) return
-
   calculator.appendNumber(number)
-  }
+}
 
+/**
+ * @param operator - The operator input as a string
+ */
 export const handleOperatorInput = (operator: string, calculator: Calculator): void => {
   if (!calculator.isOn || calculator.isInputDisabled) return
 
-  else if (!calculator.canAddOperator && operator == Operator.SUBTRACT ) {
-      calculator.appendNegative()
-      return
-    }
-
-  else if (!calculator.canAddOperator) return
-  
-  calculator.appendOperator(operator)
+/** 
+ * If the calculator can't add an operator - i.e. the last input was an operator,
+ * and the input is a subtraction operator, append a negative(-) sign instead
+ */
+  if (!calculator.canAddOperator && operator === Operator.SUBTRACT) {
+    calculator.appendNegative()
+    return
   }
+
+  if (!calculator.canAddOperator) return
+  calculator.appendOperator(operator)
+}
 
 export const handleDecimalInput = (calculator: Calculator): void => {
   if (!calculator.isOn || !calculator.canAddDecimal) return
-  
   calculator.appendDecimal()
-  }
-
-
-  
-  
-  
+}
--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,52 @@
-import { handleAC, handleBackspace, handleBye, handleDecimalInput, handleEqualInput, handleHello, handleNumberInput, handleOperatorInput } from './eventHandlers'
+import { 
+  handleAC, 
+  handleBackspace, 
+  handleBye, 
+  handleDecimalInput, 
+  handleEqualInput, 
+  handleHello, 
+  handleNumberInput, 
+  handleOperatorInput 
+} from './eventHandlers'
 import Calculator, {CalculatorProps} from './models/calculator'
 import './style.css'
 
+// Display elements
 const displayElement = <HTMLDivElement> document.querySelector<HTMLDivElement>('.display')
 const resultDisplay = <HTMLDivElement> document.getElementById('result-display')
 const expressionDisplay = <HTMLDivElement> document.getElementById('expression-display')
 
-const hello = <HTMLButtonElement> document.getElementById('hello-key')
-const bye = <HTMLButtonElement> document.getElementById('bye-key')
-
+// Calculator keys
+const hello = <HTMLButtonElement> document.getElementById('hello-key')  // SPECIAL KEY
+const bye = <HTMLButtonElement> document.getElementById('bye-key')      // SPECIAL KEY
 const allClear = <HTMLButtonElement> document.getElementById('clear-key')
 const equals = <HTMLButtonElement> document.getElementById('equal-key')
 const backspace = <HTMLButtonElement> document.getElementById('backspace-key')
 const decimal = <HTMLButtonElement> document.getElementById('decimal-key')
-
 const numbers = document.querySelectorAll<HTMLButtonElement>('.number-key')
 const operators = document.querySelectorAll<HTMLButtonElement>('.operator-key')
 
+/**
+ * Initialize the calculator and add event listeners for handling inputs.
+ */
 const initialize = (): void => {
-  const props: CalculatorProps = {displayElement, resultDisplay, expressionDisplay}
+  // Initialize calculator with references to the display elements
+  const props: CalculatorProps = { displayElement, resultDisplay, expressionDisplay }
   const calculator = new Calculator(props)
   
-  hello.addEventListener('click', () => {
-    handleHello(calculator)
-  })
-
-  bye.addEventListener('click', () => {
-    handleBye(calculator)
-  })
-
-  allClear.addEventListener('click', () => {
-    handleAC(calculator)
-  })
-
-  equals.addEventListener('click', () => {
-    handleEqualInput(calculator)
-  })
-  
-  backspace.addEventListener('click', () => {
-    handleBackspace(calculator)
-  })
+  // Add event listeners to each button for handling inputs
+  hello.addEventListener('click', () => handleHello(calculator))
+  bye.addEventListener('click', () => handleBye(calculator))
+  allClear.addEventListener('click', () => handleAC(calculator))
+  equals.addEventListener('click', () => handleEqualInput(calculator))
+  backspace.addEventListener('click', () => handleBackspace(calculator))
 
   numbers.forEach(button => button.addEventListener('click', (event: Event) => {
     const target = event.target as HTMLButtonElement
     handleNumberInput(<string>target.textContent, calculator)
   }))
 
-  decimal.addEventListener('click', () => {
-    handleDecimalInput(calculator)
-  })
+  decimal.addEventListener('click', () => handleDecimalInput(calculator))
 
   operators.forEach(button => button.addEventListener('click', (event: Event) => {
     const target = event.target as HTMLButtonElement
